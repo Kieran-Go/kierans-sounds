873efd646e275e07499cb40f8085d4a3e5834b27
Kieran-Go <187461911+Kieran-Go@users.noreply.github.com>
2025-08-19 16:36:30 +0930

    Add music player functionality to MainPlayer
    - Introduced 'songs' state and 'activeSong' tracking
    - Implement next/prev song switching with wrap-around
    - Refactor play/pause logic to handle both sounds and music player
    - Add volume control for music player seperate from sound volumes
    - Persist music and sound volumes in localStorage
    - Keep existing sound-grid functionality intact

--- client/data/mockData.js
+++ client/data/mockData.js
@@ -17,6 +17,27 @@ const mockDb = {
             url: "https://res.cloudinary.com/de7vulkpy/video/upload/v1755486726/birds_s8iu2p.mp3",
         },
     ],
+
+    songs: [
+        {
+            id: 1,
+            name: "Midnight Rendezvous",
+            author: "Final Fantasy VII Remake",
+            url: "https://res.cloudinary.com/de7vulkpy/video/upload/v1755576590/Final_Fantasy_7_Remake_Midnight_Rendezvous_a2vhtt.mp3"
+        },
+        {
+            id: 2,
+            name: "Dire Dire Docks",
+            author: "Super Mario 64",
+            url: "https://res.cloudinary.com/de7vulkpy/video/upload/v1755576723/Super_Mario_64_Dire_Dire_Docks_mkiezb.mp3"
+        },
+        {
+            id: 3,
+            name: "Sunset Haze",
+            author: "Gran Turismo 5",
+            url: "https://res.cloudinary.com/de7vulkpy/video/upload/v1755576705/Gran_Turismo_5_Sunset_Haze_p4i37k.mp3"
+        },
+    ]
 }
 
 export default mockDb;
--- client/src/components/MainPlayer.jsx
+++ client/src/components/MainPlayer.jsx
@@ -1,5 +1,5 @@
 import '../css/MainPlayer.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import playBtnImg from '../assets/images/play.svg';
 import pauseBtnImg from '../assets/images/pause.svg';
 import audioImg from '../assets/images/audio.svg';
@@ -9,6 +9,10 @@ import mockDb from '../../data/mockData';
 export default function MainPlayer() {
     const [play, setPlay] = useState(false);
     const [sounds, setSounds] = useState([]);
+    const [songs, setSongs] = useState([]);
+    const [activeSong, setActiveSong] = useState(null);
+
+    const musicPlayerAudio = useRef(new Audio);
 
     // Fetch user added data
     const data = mockDb; // Use mock data for now
@@ -18,11 +22,12 @@ export default function MainPlayer() {
         // Set document title
         document.title = "Kieran's Sounds";
 
-        // Get stored volumes
-        const storedVolumes = getStoredVolumes();
+        // Get stored sound volumes
+        const storedVolumes = getStoredSoundVolumes();
 
         // Clone local sounds and make sure they loop
         const localSounds = localData.map(sound => {
+            // Get volume data from local storage - or default to 0 if none
             const storedVolume = storedVolumes[sound.id];
             const volume = storedVolume !== undefined ? storedVolume : 0;
 
@@ -31,8 +36,9 @@ export default function MainPlayer() {
             return { ...sound, isPlaying: false, volume };
         });
 
-        // Format fetched data in the same way as local sounds
+        // Format fetched sound data in the same way as local sounds
         const userSounds = data.sounds.map(sound => {
+            // Get volume data from local storage- or default to 0 if none
             const storedVolume = storedVolumes[sound.id];
             const volume = storedVolume !== undefined ? storedVolume : 0;
 
@@ -50,11 +56,31 @@ export default function MainPlayer() {
             };
         });
 
+        // Set the sounds array using user sounds and local sounds
         setSounds([...userSounds, ...localSounds]);
+
+        // Set the songs array using the fetched data - no need to re-format here
+        setSongs(data.songs);
     },[])
 
-    // Play/Pause sounds
+    // If there are songs in the array, set the first one as active by default
     useEffect(() => {
+        if(songs.length > 0) {
+            newActiveSong(songs[0]);
+        }
+    },[songs]);
+
+    useEffect(() => {
+        if (activeSong) {
+            const audio = activeSong.audio;
+            audio.onended = () => nextSong(); // always references latest activeSong
+        }
+    }, [activeSong]);
+
+
+    // Play/Pause sounds and music player
+    useEffect(() => {
+        // Sounds
         sounds.forEach((sound) => {
             if (sound.audio.volume > 0) {
                 if (play && !sound.isPlaying) {
@@ -66,11 +92,79 @@ export default function MainPlayer() {
                 }
             }
         });
-    }, [play, sounds]);
+
+        // Music player song
+        if(activeSong && activeSong.volume > 0) {
+            if(play && !activeSong.isPlaying) {
+                activeSong.audio.play();
+                activeSong.isPlaying = true;
+            } else if (!play && activeSong.isPlaying) {
+                activeSong.audio.pause();
+                activeSong.isPlaying = false;
+            }
+        }
+    }, [play, sounds, activeSong]);
 
     // Toggles the play state
     const togglePlay = () => {
         setPlay(!play);
+    }
+
+    // Sets the song param as the new active song object
+    const newActiveSong = (song) => {
+        // Retrieve music player volume from active storage if exists
+        const storedVolume = localStorage.getItem("musicVolumeStorage");
+        const volume = storedVolume !== null ? JSON.parse(storedVolume) : (activeSong?.volume || 0);
+
+        // Swap the music player audio references' values
+        const audio = musicPlayerAudio.current;
+        audio.src = song.url; // Swap to new song source
+        audio.volume = volume;
+
+        // Retrieve isPlaying status or default to false
+        const isPlaying = activeSong?.isPlaying || false;
+        if(isPlaying) audio.play();
+
+        // Create the new active song object and set it
+        const newActiveSong = {
+            id: song.id,
+            name: song.name,
+            author: song.author,
+            audio: audio,
+            volume: volume,
+            isPlaying: isPlaying
+        }
+        setActiveSong(newActiveSong);
+    }
+
+    // Switches active song to next in array or wraps around
+    const nextSong = () => {
+        if (!activeSong || songs.length === 0) return;
+
+        // Find the index of the active song
+        const currentIndex = songs.findIndex((s) => s.id === activeSong.id);
+
+        // If current song not found, bail
+        if (currentIndex === -1) return;
+
+        // Wrap around if last song, else go to next
+        const nextIndex = (currentIndex + 1) % songs.length;
+        newActiveSong(songs[nextIndex]);
+    }
+
+    // Switches active song to prev in array or wraps arround
+    const prevSong = () => {
+        if (!activeSong || songs.length === 0) return;
+
+        // Find the index of the active song
+        const currentIndex = songs.findIndex((s) => s.id === activeSong.id);
+
+        // If current song not found, bail
+        if (currentIndex === -1) return;
+
+        // Wrap around if first song, else go to next
+        const prevIndex = (currentIndex - 1 + songs.length) % songs.length;
+        newActiveSong(songs[prevIndex]);
     }
 
     // Handle when a sound's volume is changed by user
@@ -91,13 +185,31 @@ export default function MainPlayer() {
         setSounds(newSounds);
 
         // save sound volumes to localStorage 
-        const storedVolumes = getStoredVolumes();
+        const storedVolumes = getStoredSoundVolumes();
         storedVolumes[newSounds[index].id] = value;
         localStorage.setItem("soundVolumeStorage", JSON.stringify(storedVolumes));
     }
 
-    // Function to read local storage for stored volumes
-    const getStoredVolumes = () => {
+    // Handle when the volume for the music player is changed
+   const handleMusicVolumeChange = (e) => {
+        const value = parseFloat(e.target.value);
+        if (!activeSong) return;
+
+        const newAudio = activeSong.audio;
+        newAudio.volume = value;
+
+        setActiveSong({
+            ...activeSong,
+            audio: newAudio,
+            volume: value,
+        });
+
+        // save music player volume to localStorage
+        localStorage.setItem("musicVolumeStorage", JSON.stringify(value));
+    };
+
+    // Function to read local storage for stored sound volumes
+    const getStoredSoundVolumes = () => {
         const stored = localStorage.getItem("soundVolumeStorage");
         return stored ? JSON.parse(stored) : {};
     }
@@ -108,6 +220,28 @@ export default function MainPlayer() {
         <section className="main-player__play-btn" onClick={() => togglePlay()}>
             <img src={play ? pauseBtnImg : playBtnImg}/>
         </section>
+
+        {/* Render music player */}
+        <div className='music-player'>
+            <p>MUSIC PLAYER</p>
+            <p onClick={() => nextSong()}>next</p>
+            <p onClick={() => prevSong()}>prev</p>
+            {activeSong && (
+                <p>{activeSong.name}{activeSong.author ? ` — ${activeSong.author}` : ''}</p>
+            )
+            }
+            <input
+                type="range"
+                min="0"
+                max="1"
+                step="0.01"
+                value={activeSong?.volume || 0}
+                onChange={(e) => handleMusicVolumeChange(e)}
+            />
+            <div>
+
+            </div>
+        </div>
   
         {/* Render the sound-grid */}
         <section className="main-player__sound-grid">
